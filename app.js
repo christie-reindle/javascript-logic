119956ebd1b39fe962a10f30fc982b3f02d0a4ca
creindle <creindle@hawaii.edu>
2017-03-17 14:09:44 -1000

    Finished example

--- app.js
+++ app.js
@@ -9,29 +9,38 @@
 */
 
 // Addition (+)
-5 + 9;
+console.log(5 + 9);// 14
 
-"one" + "one";
+console.log("one" + "one");// 'oneone'
 
 // Subtraction (-)
-8 - 20;
+console.log(8 - 20); // -12
 
 // Multiplication (*)
-2 * 0;
+console.log(2 * 0); //0
 
 // Division (/)
-15/50;
+console.log(15/50); //0.3
 
 // Modulus (%)
-5 % 2;
+console.log(5 % 2); // 1
 
 // Increment (++)
-2++;
+//2++; // This is invalid
+var a = 2;
+a++;
+console.log("Increment 2", a);
 
--1++;
+//-1++; // This is invalid
+var b = -1;
+b++;
+console.log("Increment -1", b);
 
 // Decrement (--)
-5--;
+//5--; // This is invalid
+ var c = 5;
+ c--;
+ console.log("Decrement 5" + c);
 
 /*
 * Comparison Operators (These return a boolean value of true or false)
@@ -39,35 +48,35 @@
 */
 
 // Strict Equal (===)
-5 === "five";
+console.log(5 === "five"); // 5 === 5, gives true
 
 // Not Equal (!==)
-5!== "five";
+console.log(5!== "five"); // true
 
 // Greater Than (>)
-5 > 10;
+console.log(5 > 10); // false - 5 > 1, gives true
 
 // Less Than (<)
-"20" < 20;
+console.log("20" < 20); // false - 19 < 20, gives true
 
 // Greater than or Equal to (>=)
-undefined >= null;
+console.log(undefined >= null); // false - null >= null, gives true
 
 // Less than or Equal to (<=)
-null <= null; 
+console.log(null <= null); // true
 
 /*
 * Logical (or Relational) Operators
 */
 
 // And (&&) - If both the operands satisfy the statment, then the condition becomes true
-1 > 0 && 1 < 0;
+console.log(1 > 0 && 1 < 0); // false - 1 > 0 && 1 < 0, true
 
 // Or (||) - If any of the two operands satisfy the statement, then the condition becomes true.
-2 > 0 || 2 > 20;
+console.log(2 > 0 || 2 > 20); // true
 
 // Not (!) - Reverses the logical state of its operand.
-!true;
+console.log(!true); // false - !false gives true
 
 /*
 * Conditionals
